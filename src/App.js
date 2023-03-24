34f18b18dcf73418f800429d84990ad48c26ff52
Sameer2244 <31188576+Sameer2244@users.noreply.github.com>
2023-03-24 23:05:07 +0530

    usecallback

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Test from './Component/Test.js';
 
 export default function App() {
   const [Testarr, setTestarr] = React.useState([1, 2, 3, 4, 5]);
-  const removeComponent = (data) => {
+  const removeComponent = React.useCallback((data) => {
     setTestarr((e) => e.filter((item) => item !== data));
-  };
+  }, []);
   return (
     <div>
       {Testarr.map((e, i) => {
