57042f3aaaf9006759512816385d8ee4065ce36e
Sameer2244 <31188576+Sameer2244@users.noreply.github.com>
2023-03-25 15:17:07 +0530

    notification-buttons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './style.css';
 import Test from './Component/Test.js';
 
 export default function App() {
-  const [Testarr, setTestarr] = React.useState([1, 2, 3, 4, 5]);
+  const [Testarr, setTestarr] = React.useState([]);
+  const counter = React.useRef(0);
   const removeComponent = React.useCallback((data) => {
     setTestarr((e) => e.filter((item) => item !== data));
   }, []);
+  const addElement = () => {
+    counter.current = counter.current + 1;
+    setTestarr((e) => [...e, `notification ${counter.current}`]);
+  };
   return (
     <div>
+      <div className="notification-buttons">
+        <div className="button info">Info</div>
+        <div className="button success">Success</div>
+        <div className="button warning">Warning</div>
+        <div className="button error">Error</div>
+      </div>
       {Testarr.map((e, i) => {
         return (
           <div key={e}>
